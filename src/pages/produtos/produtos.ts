3c2b2d05d5ea6d2d0768350769848578991356ff
Rodrigo Siqueira <rodrigo.siqueira@rdsti.com.br>
2021-03-10 21:07:28 -0300

    Infinite scroll

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -12,7 +12,7 @@ import { ProdutoService } from '../../services/domain/produto.service';
 export class ProdutosPage {
 
   items : ProdutoDTO[] = [];
-
+  page : number = 0;
 
   constructor(
     public navCtrl: NavController, 
@@ -28,19 +28,24 @@ export class ProdutosPage {
   loadData() {
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
-    this.produtoService.findByCategoria(categoria_id)
+
+    this.produtoService.findByCategoria(categoria_id, this.page, 10)
       .subscribe(response => {
-        this.items = response['content'];
+        let start = this.items.length;
+        this.items = this.items.concat(response['content']);
+        let end = this.items.length - 1;
         loader.dismiss();
-        this.loadImageUrls();
+        console.log(this.page);
+        console.log(this.items);
+        this.loadImageUrls(start, end);
       },
       error => {
         loader.dismiss();
       });
   }
 
-  loadImageUrls() {
-    for (var i=0; i<this.items.length; i++) {
+  loadImageUrls(start: number, end: number) {
+    for (var i=start; i<=end; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
         .subscribe(response => {
@@ -48,7 +53,7 @@ export class ProdutosPage {
         },
         error => {});
     }
-  } 
+  }
 
   showDetail(produto_id : string) {
     this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
@@ -64,6 +69,8 @@ export class ProdutosPage {
   }
 
   doRefresh(refresher) {
+    this.page = 0;
+    this.items = [];
     this.loadData();
 
     setTimeout(() => {
@@ -72,5 +79,13 @@ export class ProdutosPage {
     }, 1000);
   }
 
-  
+  doInfinite(infiniteScroll) {
+    this.page++;
+    this.loadData();
+
+    setTimeout(() => {
+      infiniteScroll.complete();
+    }, 1000);
+  }
+
 }
