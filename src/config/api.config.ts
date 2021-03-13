54e5a953c5c87575fa257129451d3d8f0e65e5e9
Rodrigo Siqueira <rodrigo.siqueira@rdsti.com.br>
2021-03-13 03:21:15 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
 
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://rdsti-curso-spring-ionic.herokuapp.com/",
     bucketBaseUrl: "https://curso-spring-ionic-rdsti.s3-sa-east-1.amazonaws.com"
 
 }
